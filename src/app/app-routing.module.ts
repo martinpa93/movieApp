3a3091852d5986cb2f878c711c73845c2e47f980
Martín P <martinpa_93@hotmail.com>
2019-04-24 19:15:37 +0200

    skeleton

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', loadChildren: './movies/movies.module#MoviesPageModule' },
+  { path: 'movie', loadChildren: './movie/movie.module#MoviePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
